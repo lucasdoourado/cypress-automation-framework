df39911955d24d338e3bd3e0c13e2ada31bec44d
lucasdoourado <lucas.ada95@outlook.com>
2023-05-26 14:08:18 -0300

    studying

---
+++ cypress/e2e/webdriver-uni/autocomplete-lists.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+describe("verify autocomplete dropdown list via webdriveruni", () => {
+    it("select specific product via autocomplete list", () => {
+        cy.visit("https://webdriveruniversity.com/");
+        cy.get('#autocomplete-textfield').invoke('removeAttr', 'target').click({force: true});
+        cy.get('#myInput').type('A');
+        cy.get('#myInputautocomplete-list > *').each(($el, index, $list) =>{
+            let prod = $el.text();
+            let productToSelect = 'Avacado';
+            if(prod === productToSelect){
+                $el.trigger('click');
+                cy.get('#submit-button').click();
+                cy.url().should('contain', productToSelect);
+            }
+        });
+    });
+});
