5aa2c4a73a2305b85eb5cca28d423c2d3ce12652
lucasdoourado <lucas.ada95@outlook.com>
2023-05-30 08:42:58 -0300

    adding retry

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,29 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+        baseUrl: "https://webdriveruniversity.com/",
+        chromeWebSecurity: false,
+        experimentalSessionAndOrigin: true,
+        defaultCommandTimeout: 10000,
+        pageLoadTimeout: 120000,
+        screenshotOnRunFailure: false,
+        videoUploadOnPasses: false,
+        trashAssetsBeforeRuns: true,
+        video: false,
+        reporter: 'cypress-multi-reporters',
+        reporterOptions: {
+            configFile: 'reporter-config.json',
+        },
+        retries: {
+            runMode: 1, openMode: 1
+        },
+        env: {
+            webdriveruni_homepage: "https://webdriveruniversity.com/", first_name: "Giulia"
+        }
     },
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
-    baseUrl: "https://webdriveruniversity.com/",
-    chromeWebSecurity: false,
-    experimentalSessionAndOrigin: true,
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 120000,
-    screenshotOnRunFailure: false,
-    videoUploadOnPasses: false,
-    trashAssetsBeforeRuns: true,
-    video: false,
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-      configFile: 'reporter-config.json',
-    },
-    env: {
-      webdriveruni_homepage: "https://webdriveruniversity.com/",
-      first_name: "Giulia"
-    }
-  },
 });
