403a2d6794ae5702114313b2e875bbf055dff936
lucasdoourado <lucas.ada95@outlook.com>
2023-05-30 22:35:45 -0300

    iframe.js

--- cypress/e2e/webdriver-uni/iframe.js
+++ cypress/e2e/webdriver-uni/iframe.js
@@ -1,17 +1,17 @@
-/// <reference types="Cypress" />
-describe("handling Iframes & modals", () => {
-	it("handle webdriveruni iframe and modal", () => {
-		cy.visit("https://webdriveruniversity.com/");
-		cy.get('#iframe').invoke('removeAttr', 'target').click({force: true});
-		cy.get('#frame').then(($iframe) => {
-			let body = $iframe.contents().find('body');
-			cy.wrap(body).as('iframe');
+/// <reference types="cypress" />
+describe("Handling IFrame & Modals", () => {
+	it("Handle webdriveruni iframe and modal", () => {
+		cy.visit("http://www.webdriveruniversity.com")
+		cy.get('#iframe').invoke('removeAttr', 'target').click({ force: true });
+		cy.get('#frame').then($iframe => {
+			const body = $iframe.contents().find('body')
+			cy.wrap(body).as('iframe')
 		});
 		cy.get('@iframe').find('#button-find-out-more').click();
 		cy.get('@iframe').find('#myModal').as('modal');
 		cy.get('@modal').should(($expectedText) => {
-			let text = $expectedText.text();
-			expect(text).to.include('Welcome to webdriveracademy.com');
+			const text = $expectedText.text()
+			expect(text).to.include('Welcome to webdriveruniversity.com we sell a wide range of electrical goods');
 		});
 		cy.get('@modal').contains('Close').click();
 	});
