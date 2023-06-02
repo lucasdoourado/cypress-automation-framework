eb2dc27632647916ad447e1dd0cd9b558245077e
lucasdoourado <lucas.ada95@outlook.com>
2023-06-02 18:18:33 -0300

    adding jenkinsfiel

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'sa123c',
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
