6093b88c6f623ad6d5e840c1284dc0ea83cc562b
lucasdoourado <lucas.ada95@outlook.com>
2023-10-26 09:20:20 -0300

    tests

--- cypress/e2e/network-request.cy.js
+++ cypress/e2e/network-request.cy.js
@@ -1,49 +1,60 @@
 /// <reference types="cypress"/>
-describe('network requests', () => {
-  const comment = 'Unable to find comment!';
+describe("network requests", () => {
+  const comment = "Unable to find comment!";
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/network-requests');
+    cy.visit("https://example.cypress.io/commands/network-requests");
   });
-  it('GET request', () => {
-    cy.intercept({
-      method: 'GET',
-      url: '**/comments/*',
-    },
+  it("GET request", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: "**/comments/*",
+      },
       {
         body: {
           postId: 1,
           id: 1,
           name: "test name 123",
           email: "test_123@email.com",
-          body: "testing a xhr request"
-        }
-      }).as('getComment');
-    cy.get('.network-btn').click();
-    cy.wait('@getComment').its('response.statusCode').should('eq', 200);
+          body: "testing a xhr request",
+        },
+      }
+    ).as("getComment");
+    cy.get(".network-btn").click();
+    cy.wait("@getComment").its("response.statusCode").should("eq", 200);
   });
-  it('POST request', () => {
-    cy.intercept('POST', '/comments').as('postComments');
-    cy.get('.network-post').click();
-    cy.wait('@postComments').should(({ request, response }) => {
-      expect(request.body).to.include('email');
-      expect(request.headers).to.have.property('content-type', 'application/x-www-form-urlencoded; charset=UTF-8');
-      expect(response.body).to.have.property('name', 'Using POST in cy.intercept()');
+  it("POST request", () => {
+    cy.intercept("POST", "/comments").as("postComments");
+    cy.get(".network-post").click();
+    cy.wait("@postComments").should(({ request, response }) => {
+      expect(request.body).to.include("email");
+      expect(request.headers).to.have.property(
+        "content-type",
+        "application/x-www-form-urlencoded; charset=UTF-8"
+      );
+      expect(response.body).to.have.property(
+        "name",
+        "Using POST in cy.intercept()"
+      );
     });
   });
-  it('PUT request', () => {
-    cy.intercept({
-      method: 'PUT',
-      url: "**/comments/*"
-    },
+  it("PUT request", () => {
+    cy.intercept(
+      {
+        method: "PUT",
+        url: "**/comments/*",
+      },
       {
         statusCode: 404,
         body: {
-          error: comment
+          error: comment,
         },
-        delay: 500
-      }).as('putComment');
-    cy.get('.network-put').click();
-    cy.wait('@putComment');
-    cy.get('.network-put-comment').should('contain', comment);
+        delay: 500,
+      }
+    ).as("putComment");
+    cy.get(".network-put").click();
+    cy.wait("@putComment");
+    cy.get(".network-put-comment").should("contain", comment);
   });
+  it("should ", () => {});
 });
--- cypress/e2e/webdriver-uni/dropdown-menu.js
+++ cypress/e2e/webdriver-uni/dropdown-menu.js
@@ -1,15 +1,24 @@
 /// <reference types="Cypress" />
 describe("interact with dropdown list via webdriveruni", () => {
-	it("select specific values", () => {
-		cy.visit("https://webdriveruniversity.com/");
-		cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force: true });
-		cy.get('#dropdowm-menu-1').select('c#').should('have.value', 'c#'); // select via value
-		cy.get('#dropdowm-menu-2').select('Maven').contains('Maven'); // select via text
-		cy.get('#dropdowm-menu-3').select(3); // select via index
-	});
-	it("validate specific states", () => {
-		cy.visit("https://webdriveruniversity.com/");
-		cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force: true });
-		cy.get('#fruit-selects').find('[value=\'orange\']').should('have.attr', 'disabled');
-	});
+  it("select specific values", () => {
+    cy.visit("https://webdriveruniversity.com/");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get("#dropdowm-menu-1").select("c#").should("have.value", "c#"); // select via value
+    cy.get("#dropdowm-menu-2").select("Maven").contains("Maven"); // select via text
+    cy.get("#dropdowm-menu-3").select(3); // select via index
+    cy.get("#dropdowm-menu-1").each(($el, index, $list) => {
+      cy.log("\n" + $el.text() + "\n");
+    });
+  });
+  it("validate specific states", () => {
+    cy.visit("https://webdriveruniversity.com/");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get("#fruit-selects")
+      .find("[value='orange']")
+      .should("have.attr", "disabled");
+  });
 });
